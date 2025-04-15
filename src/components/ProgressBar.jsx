bd4d62c31c58ed944b8c83d89f651ba5366aa9d1
wholesoulworks <wholesoulworks@gmail.com>
2025-04-15 16:53:07 -0400

    Update ProgressBar.jsx

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,1 +1,10 @@
-
+export default function ProgressBar({ progress }) {
+  return (
+    <div className="w-full h-1 bg-gray-100 mb-12">
+      <div
+        className="h-full bg-orange-500 transition-all duration-500 ease-in-out"
+        style={{ width: `${progress}%` }}
+      />
+    </div>
+  );
+}
