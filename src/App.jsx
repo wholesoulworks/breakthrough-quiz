8e29ef596a291aab08189e7e218b51d8aa012718
wholesoulworks <wholesoulworks@gmail.com>
2025-04-16 17:51:08 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Quiz from './components/Quiz';
+import AppRoutes from './Routes';
 
 export default function App() {
   return (
-    <main className="min-h-screen bg-[#0D0D0D] text-[#F4F4F4] flex items-center justify-center p-4">
-      <Quiz />
+    <main className="min-h-screen bg-[#0D0D0D] text-[#F4F4F4]">
+      <AppRoutes />
     </main>
   );
 }
