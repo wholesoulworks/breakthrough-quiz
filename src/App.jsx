fe775700d9d3b9a30cbb39783a5e8bec0f9d30d3
wholesoulworks <wholesoulworks@gmail.com>
2025-04-17 11:38:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-white bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello from a clean start 👋🏽</h1>
+    </div>
+  );
+}
+
+export default App;
 
