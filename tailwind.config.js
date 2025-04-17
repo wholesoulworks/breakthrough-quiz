93855f1f42875d8c94be0210546e4eb49a10f82d
wholesoulworks <wholesoulworks@gmail.com>
2025-04-17 19:53:49 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 };
